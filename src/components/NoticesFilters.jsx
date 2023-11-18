bc961299f218396ebd7705d22483e7565b85aee4
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-18 23:14:23 +0200

    Update NoticesFilters.jsx

--- src/components/NoticesFilters.jsx
+++ src/components/NoticesFilters.jsx
@@ -7,7 +7,16 @@ const NoticesFilters = () => {
   return (
     <div>
       <h4>Filters</h4>
-      {isByAgeOpen ? <button>By age</button> : <div></div>}
+      {isByAgeOpen ? (
+        <button>By age</button>
+      ) : (
+        <div>
+          By age
+          <label>up to 1 year</label>
+          <label>up to 2 years</label>
+          <label>from 2 years</label>
+        </div>
+      )}
       {isByGenderOpen ? <button>By gender</button> : <div></div>}
     </div>
   );
