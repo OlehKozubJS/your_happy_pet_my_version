7b0f14023fd59c2f70f2fefccffda16422b9599d
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-19 18:35:21 +0200

    Update NoticesCategoriesNav.jsx

--- src/components/NoticesCategoriesNav.jsx
+++ src/components/NoticesCategoriesNav.jsx
@@ -3,3 +3,15 @@
 // in good hands
 // favorite ads
 // my ads
+
+const NoticesCategoriesNav = () => {
+  return (
+    <div>
+      <div>sell</div>
+      <div>lost/found</div>
+      <div>in good hands</div>
+      <div>favorite ads</div>
+      <div>my ads</div>
+    </div>
+  );
+};
