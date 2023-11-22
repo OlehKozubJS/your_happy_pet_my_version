9c1d60c5ea351cfc561b6db549bbb345b2893a9d
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-22 22:10:13 +0200

    Update NoticesFilters.jsx

--- src/components/NoticesFilters/NoticesFilters.jsx
+++ src/components/NoticesFilters/NoticesFilters.jsx
@@ -6,7 +6,7 @@ const NoticesFilters = ({ onChange }) => {
   const [isFiltersOpen, setIsFiltersOpen] = useState(false);
   const [isByAgeOpen, setIsByAgeOpen] = useState(false);
   const [isByGenderOpen, setIsByGenderOpen] = useState(false);
-  const [age, detAge] = useState();
+  const [age, setAge] = useState(0);
   const [gender, setGender] = useState('female');
 
   const toggleFilters = () => {
