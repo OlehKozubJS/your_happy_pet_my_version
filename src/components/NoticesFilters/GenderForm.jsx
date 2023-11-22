3e194f2c88ed83b38da4960284457288a354229c
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-22 21:11:56 +0200

    Update GenderForm.jsx

---
+++ src/components/NoticesFilters/GenderForm.jsx
@@ -0,0 +1,14 @@
+const GenderForm = () => {
+  return (
+    <form>
+      <label>
+        <input type="checkbox" />
+        female
+      </label>
+      <label>
+        <input type="checkbox" />
+        male
+      </label>
+    </form>
+  );
+};
