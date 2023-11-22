0be82415779a6e0bb2c70c6f43017f8bd76a74f6
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-22 21:20:49 +0200

    Update AgeForm.jsx

--- src/components/NoticesFilters/AgeForm.jsx
+++ src/components/NoticesFilters/AgeForm.jsx
@@ -1,16 +1,16 @@
-const AgeForm = () => {
+const AgeForm = ({ onChange }) => {
   return (
     <form>
       <label>
-        <input type="checkbox" />
+        <input type="checkbox" name="ageSelect" />
         up to 1 year
       </label>
       <label>
-        <input type="checkbox" />
+        <input type="checkbox" name="ageSelect" />
         up to 2 years
       </label>
       <label>
-        <input type="checkbox" />
+        <input type="checkbox" name="ageSelect" />
         from 2 years
       </label>
     </form>
