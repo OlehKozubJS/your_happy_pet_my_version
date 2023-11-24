cb6f51eaa44da5a8a901e803e0efb716f9520d62
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-24 23:28:59 +0200

    Update AgeForm.jsx

--- src/components/NoticesFilters/AgeForm.jsx
+++ src/components/NoticesFilters/AgeForm.jsx
@@ -1,32 +1,26 @@
 const AgeForm = ({ onChange }) => {
   const handleInput = event => {
     event.preventDefault();
-    const { upToOneOption, upToTwoOption, fromTwoOption } =
-      event.currentTarget.elements;
-    const ageData = {
-      upToOne: upToOneOption.checked,
-      upToTwo: upToTwoOption.checked,
-      fromTwo: fromTwoOption.checked,
-    };
+    const ageData = event.currentTarget.elements.ageOption.value;
     onChange(ageData);
   };
 
   return (
     <form onInput={handleInput}>
       <label>
-        <input type="checkbox" name="upToOneOption" />
+        <input type="radio" name="ageOption" value="1-" />
         up to 1 year
       </label>
       <label>
-        <input type="checkbox" name="upToTwoOption" />
+        <input type="radio" name="ageOption" value="2-" />
         up to 2 years
       </label>
       <label>
-        <input type="checkbox" name="fromTwoOption" />
+        <input type="radio" name="ageOption" value="2+" />
         from 2 years
       </label>
       <label>
-        <input type="checkbox" name="anyAgeOption" />
+        <input type="radio" name="ageOption" value="" />
         any age
       </label>
     </form>
