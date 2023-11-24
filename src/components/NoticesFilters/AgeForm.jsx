4910e03f59c2459bf1e3b0ec7ca666bc2191ac87
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-24 23:31:35 +0200

    Update AgeForm.jsx

--- src/components/NoticesFilters/AgeForm.jsx
+++ src/components/NoticesFilters/AgeForm.jsx
@@ -8,19 +8,19 @@ const AgeForm = ({ onChange }) => {
   return (
     <form onInput={handleInput}>
       <label>
-        <input type="radio" name="ageOption" value="1-" />
+        <input type="radio" name="ageOption" value="-1" />
         up to 1 year
       </label>
       <label>
-        <input type="radio" name="ageOption" value="2-" />
+        <input type="radio" name="ageOption" value="-2" />
         up to 2 years
       </label>
       <label>
-        <input type="radio" name="ageOption" value="2+" />
+        <input type="radio" name="ageOption" value="+2" />
         from 2 years
       </label>
       <label>
-        <input type="radio" name="ageOption" value="0+" />
+        <input type="radio" name="ageOption" value="+0" />
         any age
       </label>
     </form>
