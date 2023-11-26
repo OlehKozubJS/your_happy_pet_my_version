e24b0a3990a2135e278d5d1f7ffa13bedc3e5457
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-26 16:07:36 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,9 @@
+import NoticesPage from './pages/NoticesPage/NoticesPage';
+
 export const App = () => {
   return (
-    <>
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            {/* <Route index element={<MainPage />} />
-            <Route path="main" element={<MainPage />} />
-            <Route path="news" element={<NewsPage />} />
-            <Route path="notices" element={<NoticesPage />}>
-              <Route index element={<NoticesCategoriesList />} />
-              <Route path="sell" element={<NoticesCategoriesList />} />
-              <Route path="lost-found" element={<NoticesCategoriesList />} />
-              <Route path="for-free" element={<NoticesCategoriesList />} />
-              <Route path="favorite" element={<NoticesCategoriesList />} />
-              <Route path="own" element={<NoticesCategoriesList />} />
-            </Route>
-            <Route path="friends" element={<OurFriendsPage />} />
-            <Route element={<PublicRoute />}>
-              <Route path="register" element={<RegisterPage />} />
-              <Route path="login" element={<LoginPage />} />
-            </Route>
-            <Route element={<PrivateRoute />}>
-              <Route path="user" element={<UserPage />} />
-              <Route path="add-pet" element={<AddPetPage />} />
-            </Route>
-            <Route path="*" element={<NotFoundPage />} /> */}
-          </Route>
-        </Routes>
-      </Suspense>
-    </>
+    <div>
+      <NoticesPage />
+    </div>
   );
 };
