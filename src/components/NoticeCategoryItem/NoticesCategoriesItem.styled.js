2de5ca41fe7a29b048c7d18d923d10a63f98b187
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-26 15:11:20 +0200

    Update NoticesCategoriesItem.styled.js

--- src/components/NoticeCategoryItem/NoticesCategoriesItem.styled.js
+++ src/components/NoticeCategoryItem/NoticesCategoriesItem.styled.js
@@ -1,3 +1,17 @@
 import styled from '@emotion/styled';
 import { theme } from 'utils/theme';
 const { fonts, fontSizes, fontWeights, colors /*, media*/ } = theme;
+
+const CategoryItem = styled.div``;
+
+const CategoryItemHeader = styled.div``;
+
+const CategoryItemFooter = styled.div``;
+
+const CategoryItemImage = styled.img``;
+
+const CategoryOutput = styled.div``;
+
+const AddButton = styled.button``;
+
+const RemoveButton = styled.button``;
