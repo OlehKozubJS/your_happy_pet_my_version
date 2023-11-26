c9278fb97bda539255dc6659957ae62281ff1c15
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-26 16:40:49 +0200

    finalize main files and add styled-components module

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import NoticesPage from './pages/NoticesPage/NoticesPage';
+import { Suspense } from 'react';
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const NoticesPage = lazy(() => import('./pages/NoticesPage/NoticesPage'));
 
 export const App = () => {
   return (
-    <div>
-      <NoticesPage />
-    </div>
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<NoticesPage />}></Route>
+        </Routes>
+      </Suspense>
+    </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/your_happy_pet">
+        <BrowserRouter basename="/your_happy_pet_my_version">
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>
