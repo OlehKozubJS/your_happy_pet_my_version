7d372bae8b3bca732bff2789b49c72d0a33f9432
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-26 16:30:37 +0200

    npm i axios

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './utils/theme.js';
+import { App } from './App.jsx';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store.js';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/your_happy_pet">
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
