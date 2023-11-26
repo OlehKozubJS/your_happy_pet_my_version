846b214d31e7b61bc1cfffc152ea884d00e6ff1c
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-26 16:51:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './utils/theme.js';
