d19e75fc80bff053141aea77a5f6a942929aa81d
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-27 18:39:11 +0200

    Update NoticesCategoriesItem.jsx

--- src/components/NoticeCategoryItem/NoticesCategoriesItem.jsx
+++ src/components/NoticeCategoryItem/NoticesCategoriesItem.jsx
@@ -1,6 +1,3 @@
-//import { useState } from 'react';
-//import { Link } from 'react-router-dom';
-//import { HeartIcon } from '../../utils/icon';
 import icons from '../../images/symbol-defs.svg';
 
 import {
