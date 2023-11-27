1b4bb96fbca4eac0b2d5ed4c696cecb386b280e9
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-27 18:55:53 +0200

    Update NoticesCategoriesList.styled.js

---
+++ src/components/NoticesCategoriesList/NoticesCategoriesList.styled.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+const CategoriesList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justfy-content: space-around;
+  align-content: space-evenly;
+`;
