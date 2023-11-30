9aed47951c3e48f1164dd761b54ba79848983f81
OlehKozubJS <122114245+OlehKozubJS@users.noreply.github.com>
2023-11-30 01:09:26 +0000

    Deploying to gh-pages from @ OlehKozubJS/your_happy_pet_my_version@ac5e911d1eced9a7d65aeee2ce51a80d8a53e705 🚀

--- assets/MainPage-88b8ca53.js
+++ assets/MainPage-88b8ca53.js
@@ -1,4 +1,4 @@
-import{s as i,j as t}from"./index-440b9c34.js";const e="https://ivanfendrykov.github.io/your_happy_pet/assets/right-animals-ce7dfe95.png",o=i.div`
+import{s as i,j as t}from"./index-3e6d1cfa.js";const e="https://ivanfendrykov.github.io/your_happy_pet/assets/right-animals-ce7dfe95.png",o=i.div`
     display: flex;
     justify-content:center;
     height: 100vh;
